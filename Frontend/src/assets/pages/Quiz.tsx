1dc7521801fe75cdd6e725c0d72666b70a82728d
cj05 <cjcoolsuper@gmail.com>
2024-08-17 14:05:47 +0700

    quiz hooked, for what had already done

--- Frontend/src/assets/pages/Quiz.tsx
+++ Frontend/src/assets/pages/Quiz.tsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Card from "../../components/Card"
 
 function Quiz() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    
+    
+    async function RequestData() {
+      let subject = searchParams.get("subject") ?? "math"
+      let syllabus = searchParams.get("syllabus") ?? "algebra"
+      let id = searchParams.get("id") ?? "exercise_2"
+      let fetchedQuiz: any = await (await fetch(`/api/getQuizContent?subject=${subject}&syllabus=${syllabus}&id=${id}`)).json()
+
+      let quizName:string = fetchedQuiz.name
+      let quizDescription:string = fetchedQuiz.description
+
+      setTitle(quizName)
+      setDescription(quizDescription)
+    }
+    RequestData();
+  }, []);
+  const [title,setTitle] = useState("")
+  const [description,setDescription] = useState("")
+
+
   return (
     <div className="flex flex-col mx-20 my-4 font-default">
       <div>
@@ -11,7 +35,7 @@ function Quiz() {
       <div className="flex mt-4 justify-between items-end">
         <div>
           <h1 className=" text-[#0279D4] font-bold text-6xl m-auto">
-            Question Topic
+            {title}
           </h1>
         </div>
         <div className="flex gap-4 align-middle font-bold">
@@ -25,7 +49,7 @@ function Quiz() {
       </div>
 
       <div className="mt-3 pl-6">
-        <h2 className="text-2xl text-[#8A8A8A]">descriptions</h2>
+        <h2 className="text-2xl text-[#8A8A8A]">{description}</h2>
       </div>
       <div className="mt-3">
         <Card />
